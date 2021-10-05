9ac5f3c8d133da5969a73dd478dd940f7a738b72
Loureto <loureto.tadss123@gmail.com>
2021-10-04 23:31:00 -0400

    Inserindo o Axios

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+
+import { GlobalStyles } from "./styles/global";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
