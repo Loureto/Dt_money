ce8dca0d1669c80ae9d004c26d4ec5f4f233d2b4
Loureto <loureto.tadss123@gmail.com>
2021-10-09 14:34:01 -0400

    Funcionamento dos botões e cores para os mesmos

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,16 +1,19 @@
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { useState } from "react";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
-export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps) {
+export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps){
+    const [type, setType] = useState('deposit');
+
     return(
         <Modal 
         isOpen={isOpen}
@@ -39,14 +42,25 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
             />
 
             <TransactionTypeContainer>
-                <button type="button">
+                <RadioBox 
+                type="button"
+                onClick={() => { setType('deposit'); }}
+                isActive={type === 'deposit'}
+                activeColor='green'
+                >
                     <img src={incomeImg} alt="Entrada" />
                     <span>Entrada</span>                
-                </button>
-                <button type="button">
+                </RadioBox>
+
+                <RadioBox 
+                type="button"
+                onClick={() => { setType('withdraw'); }}
+                isActive={type === 'withdraw'}
+                activeColor='red'
+                >
                     <img src={outcomeImg} alt="Saída" />
                     <span>Saída</span>
-                </button>
+                </RadioBox>
             </TransactionTypeContainer>
 
             <input 
